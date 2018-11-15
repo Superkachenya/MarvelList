30a6b60ecedc7ddd6f179c6b45e38c362633aa84
Danil Voitenko <daniil@scanbot.io>
2018-11-15 17:46:43 +0100

    Initial Commit

---
+++ Marvel List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Marvel List
+//
+//  Created by Danil Voitenko on 15.11.18.
+//  Copyright © 2018 Danil Voitenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
